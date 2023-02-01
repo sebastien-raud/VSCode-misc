ea2cb405fb9e7db72c6a25e765f1197b6ad84be0
Sébastien Raud <104756443+sebastien-raud@users.noreply.github.com>
2023-02-01 17:18:59 +0100

    Update setter.js

--- phpGettersSetters/setter.js
+++ phpGettersSetters/setter.js
@@ -3,7 +3,6 @@ module.exports = (property) => `
 	 * ${property.setterDescription()}
 	 *
 	 * @param   ${property.getType() ? property.getType() : 'mixed'}  \$${property.getName()}  ${property.getDescription() ? property.getDescription() : ''}
-	 *
 	 * @return  self
 	 */
 	public function ${property.setterName().replace(/_([A-Za-z])/g, function (g) { return g[1].toUpperCase(); })}(${property.getTypeHint() ? property.getTypeHint() + ' ' : '' }\$${property.getName()})
