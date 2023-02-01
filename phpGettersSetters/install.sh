ef978068222f9d35a8289599fbbd3ab01dd13c30
Sébastien Raud <104756443+sebastien-raud@users.noreply.github.com>
2023-02-01 17:03:27 +0100

    Update install.sh

--- phpGettersSetters/install.sh
+++ phpGettersSetters/install.sh
@@ -1,16 +1,16 @@
 # !/bin/sh
 
-if [ !-d "~/.config/Code/User/phpGettersSetters" ]
+if [ ! -d "~/.config/Code/User/phpGettersSetters" ]
 then
   mkdir "~/.config/Code/User/phpGettersSetters"
 fi
 
-if [ !-d "~/.config/Code/User/phpGettersSetters" ]
+if [ ! -d "~/.config/Code/User/phpGettersSetters" ]
 then
   echo "L'extension php-getters-setters ne doit pas être installée"
   exit
 fi
 
 cd "~/.config/Code/User/phpGettersSetters"
-wget https://github.com/sebastien-raud/VSCode-misc/phpGettersSetters/getter.js
-wget https://github.com/sebastien-raud/VSCode-misc/phpGettersSetters/setter.js
+wget https://raw.githubusercontent.com/sebastien-raud/VSCode-misc/main/phpGettersSetters/getter.js
+wget https://raw.githubusercontent.com/sebastien-raud/VSCode-misc/main/phpGettersSetters/setter.js
